b350531c11adda6980002c057d689f6901b4ea68
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-01-11 22:33:51 -0600

    Completed assignment

--- ruby/selection_sort.rb
+++ ruby/selection_sort.rb
@@ -1,5 +1,12 @@
-def selection_sort(arr)
-  # type your code in here
+def selection_sort(originalArr)
+  arr = originalArr.dup
+  returnedArr = []
+  originalArr.each do |n|
+    min = arr.min
+    returnedArr.push min
+    arr.delete_at(arr.index min)
+  end
+  returnedArr
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -11,11 +18,41 @@ if __FILE__ == $PROGRAM_NAME
 
   # Don't forget to add your own!
 
+  puts "Expecting: [-9, 0, 3, 8, 10]"
+  print "=> "
+  print selection_sort([8, 3, -9, 0, 10])
+
   # BENCHMARK HERE, you can print the average runtime
   long_input = []
 
   100.times { long_input << rand }
+
+  before = Time.now
+  selection_sort long_input
+  after = Time.now
+
+  my_time = after - before
+
+  before = Time.now
+  long_input.sort
+  after = Time.now
+
+  comp_time = after - before
+
+  puts "My time: #{my_time} seconds"
+  puts "Comp time: #{comp_time} seconds"
+
 end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+
+
+=begin 
+Let's say we have an array with a length of 8
+loop 8 times
+    find the minimum of array in range i...8
+    push that number to my new array
+
+return new array
+=end
